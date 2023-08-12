236d42fd4df84002309cd7abd7d798dbfb46d31d
askininan <askin.inan@outlook.com>
2023-08-12 16:46:18 +0300

    Added new general Az API invoke function

--- Az-gh-bulk-branch/Bulk-Branches.ps1
+++ Az-gh-bulk-branch/Bulk-Branches.ps1
@@ -16,3 +16,23 @@ param(
     [alias('auto')]
     [switch]$auto_flow
 )
+
+
+##### UTILITY FUNCTIONS #####
+
+# Invoke Azure Devops API release URI
+function Invoke_AZ_API{
+    Param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Add_URI,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $METHOD,
+        [Parameter(Mandatory=$false, Position=2)]
+        [string] $requestBody
+    )
+
+    $AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)"))}
+    $uri = "$($AZBaseURL)$($Add_URI)"
+    Invoke-RestMethod -Uri $uri -Method $METHOD -body $requestBody -Headers $AzureDevOpsAuthenicationHeader -ContentType 'application/json' -ErrorAction SilentlyContinue
+}
